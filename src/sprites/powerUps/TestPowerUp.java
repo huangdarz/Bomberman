372ea6c78c766ad698987cb1c4f397de98e4b1ad
Mojo156 <legovichm20@barker.college>
2019-06-14 11:39:36 +1000

    Added random variables

--- src/sprites/powerUps/TestPowerUp.java
+++ src/sprites/powerUps/TestPowerUp.java
@@ -1,10 +1,22 @@
 package sprites.powerUps;
 
+import java.util.Random;
+
 public class TestPowerUp {
+	int randomLocationX;
+	int randomLocationY;
 	
+	Random rand = new Random();
 	
-	private void randomLocation() {
+	private void locationGeneration() {
+		randomLocationX = rand.nextInt(13);
+		randomLocationY = rand.nextInt(11);
+	}
+	
+	private void checkLocationFree() {
+		switch (randomLocationX) {
 		
+		}
 	}
 }
 
