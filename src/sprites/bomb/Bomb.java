853e129260ff7ca377a13d26b5f241c94da63b3f
huangdarz <31122258+huangdarz@users.noreply.github.com>
2019-06-22 20:30:07 +1000

    Cleaned up exploding to be using methods with parameters instead of replicating the same code in each if statement

--- src/sprites/bomb/Bomb.java
+++ src/sprites/bomb/Bomb.java
@@ -20,6 +20,8 @@ public class Bomb extends Sprite {
 	private static int maxNumBombs = 1;
 	private static int bombsPlaced = 0;
 	
+	ArrayList<Sprite> activeSprites = new ArrayList<Sprite>();
+	
 	public Bomb(Scene scene) {
 		super(scene);
 		Image image = new Image("/res/bomb.png");
@@ -36,85 +38,31 @@ public class Bomb extends Sprite {
 	public void explode() {
 		int multiplier = 1;
 		ArrayList<Explosion> blasts = new ArrayList<Explosion>();
-		ArrayList<Sprite> activeSprites = new ArrayList<Sprite>();
 		for (int x = 0; x < range*4; x++) {
 			blasts.add(new Explosion(getScene()));
 			if (x < range) {
-//				blasts.get(x).relocate(getLayoutX()-getFitWidth()*multiplier, getLayoutY());
-				if (boundExplosion) {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX()-getFitWidth()*multiplier)/50, (int)getLayoutY()/50);
-					boundExplosion = false;
-				}
-				System.out.println("Grid X: "+(int)(getLayoutX()-getFitWidth()*multiplier)/50);
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkBounds((getLayoutX()-getFitWidth()*multiplier)/50, getLayoutY()/50, blasts.get(x));
 				blasts.get(x).relocate(getLayoutX()-getFitWidth()*multiplier, getLayoutY());
-				try {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX()-getFitWidth()*multiplier)/50, (int)getLayoutY()/50);
-				} catch(ArrayIndexOutOfBoundsException e) {
-					blasts.get(x).setShouldExplode(false);
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkExplosionGrid((getLayoutX()-getFitWidth()*multiplier)/50, getLayoutY()/50, blasts.get(x));
+				checkEmpty(blasts.get(x));
 				if (x == range - 1) boundExplosion = true;
 			} else if (x < range*2) {
-//				blasts.get(x).relocate(getLayoutX(), getLayoutY()-getFitHeight()*multiplier);
-				if (boundExplosion) {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX())/50, (int)(getLayoutY()-getFitHeight()*multiplier)/50);
-					boundExplosion = false;
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkBounds((getLayoutX())/50, (getLayoutY()-getFitHeight()*multiplier)/50, blasts.get(x));
 				blasts.get(x).relocate(getLayoutX(), getLayoutY()-getFitHeight()*multiplier);
-				try {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX())/50, (int)(getLayoutY()-getFitHeight()*multiplier)/50);
-				} catch(ArrayIndexOutOfBoundsException e) {
-					blasts.get(x).setShouldExplode(false);
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkExplosionGrid((getLayoutX())/50, (getLayoutY()-getFitHeight()*multiplier)/50, blasts.get(x));
+				checkEmpty(blasts.get(x));
 				if (x == range*2 - 1) boundExplosion = true;
 			} else if (x < range*3) {
-//				blasts.get(x).relocate(getLayoutX()+getFitWidth()*multiplier, getLayoutY());
-				if (boundExplosion) {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX()+getFitWidth()*multiplier)/50, (int)getLayoutY()/50);
-					boundExplosion = false;
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkBounds((getLayoutX()+getFitWidth()*multiplier)/50, getLayoutY()/50, blasts.get(x));
 				blasts.get(x).relocate(getLayoutX()+getFitWidth()*multiplier, getLayoutY());
-				try {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX()+getFitWidth()*multiplier)/50, (int)getLayoutY()/50);
-				} catch(ArrayIndexOutOfBoundsException e) {
-					blasts.get(x).setShouldExplode(false);
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkExplosionGrid((getLayoutX()+getFitWidth()*multiplier)/50, getLayoutY()/50, blasts.get(x));
+				checkEmpty(blasts.get(x));
 				if (x == range*3 - 1) boundExplosion = true;
 			} else {
-//				blasts.get(x).relocate(getLayoutX(), getLayoutY()+getFitHeight()*multiplier);
-				if (boundExplosion) {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX())/50, (int)(getLayoutY()+getFitHeight()*multiplier)/50);
-					boundExplosion = false;
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkBounds((getLayoutX())/50, (int)(getLayoutY()+getFitHeight()*multiplier)/50, blasts.get(x));
 				blasts.get(x).relocate(getLayoutX(), getLayoutY()+getFitHeight()*multiplier);
-				try {
-					activeSprites = GameScene.getInGrid((int)(getLayoutX())/50, (int)(getLayoutY()+getFitHeight()*multiplier)/50);
-				} catch(ArrayIndexOutOfBoundsException e) {
-					blasts.get(x).setShouldExplode(false);
-				}
-				if (!activeSprites.isEmpty()) {
-					blasts.get(x).setShouldExplode(false);
-				}
+				checkExplosionGrid((getLayoutX())/50, (getLayoutY()+getFitHeight()*multiplier)/50, blasts.get(x));
+				checkEmpty(blasts.get(x));
 				if (x == range*4 - 1) boundExplosion = true;
 			}
 			blasts.get(x).toBack();
@@ -124,29 +72,60 @@ public class Bomb extends Sprite {
 		blasts.add(new Explosion(getScene()));
 		blasts.get(range*4).relocate(getLayoutX(), getLayoutY());
 		
-		LinkedList<Explosion> toExplode = new LinkedList<Explosion>();
-		for (Explosion e : blasts) {
-			if (e.getShouldExplode()) {
-				toExplode.add(e);
-			}
-		}
+		LinkedList<Explosion> toExplode = createToExplode(blasts);
 		
 		Platform.runLater(() -> {
 			getPane().getChildren().addAll(toExplode);
 			getPane().getChildren().remove(this);
 		});
 		
+		timer(750);
+		
+		Platform.runLater(() -> getPane().getChildren().removeAll(blasts));
+		setBombsPlaced(getBombsPlaced()-1);
+	}
+	
+	private void checkBounds(double x, double y, Explosion e) {
+		if (boundExplosion) {
+			activeSprites = GameScene.getInGrid((int)x, (int)y);
+			boundExplosion = false;
+		}
+		checkEmpty(e);
+	}
+	
+	private void checkEmpty(Explosion e) {
+		if (!activeSprites.isEmpty()) {
+			e.setShouldExplode(false);
+		}
+	}
+	
+	private void checkExplosionGrid(double x, double y, Explosion ex) {
+		try {
+			activeSprites = GameScene.getInGrid((int)x, (int)y);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			ex.setShouldExplode(false);
+		}
+	}
+	
+	private LinkedList<Explosion> createToExplode(ArrayList<Explosion> b) {
+		LinkedList<Explosion> toExplode = new LinkedList<Explosion>();
+		for (Explosion e : b) {
+			if (e.getShouldExplode()) {
+				toExplode.add(e);
+			}
+		}
+		return toExplode;
+	}
+	
+	private void timer(int timeMillis) {
 		long startTime = System.currentTimeMillis();
 		long finishTime = System.currentTimeMillis();
 		long elapsedTime = finishTime - startTime;
 		
-		while(elapsedTime < 750) {
+		while(elapsedTime < timeMillis) {
 			finishTime = System.currentTimeMillis();
 			elapsedTime = finishTime - startTime;
 		}
-		
-		Platform.runLater(() -> getPane().getChildren().removeAll(blasts));
-		setBombsPlaced(getBombsPlaced()-1);
 	}
 	
 	public static int getBombsPlaced() {return bombsPlaced;}
